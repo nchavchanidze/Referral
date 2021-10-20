be80929d4f7840cd3f93444251833215c327163e
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-10-20 13:31:49 +0400

    Add Route to Home

--- src/Pages/Confucius.jsx
+++ src/Pages/Confucius.jsx
@@ -1,130 +1,135 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Particles from "react-particles-js";
 const Confucius = () => {
   return (
-    <ParticlesContainer>
-      <Quote>
-        <WordCaps>"T</WordCaps>he man who moves a
-        <MainWord className="word-1"> mountain</MainWord> <br />
-        &nbsp;<WordCaps className="caps-2">b</WordCaps>egins by carrying away small
-        <MainWord className="word-2"> stones</MainWord>
-        <WordCaps>"</WordCaps>
-      </Quote>
-      <p>- confucius</p>
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 80,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: "#c5a47e",
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 0,
-                color: "#000000",
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-              image: {
-                src: "img/github.svg",
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 0.5,
-              random: false,
-              anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 40,
-                size_min: 0.1,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: true,
-              distance: 192.40944730386272,
-              color: "#c5a47e",
-              opacity: 1,
-              width: 0.6,
-            },
-            move: {
-              enable: true,
-              speed: 3,
-              direction: "none",
-              random: false,
-              straight: false,
-              out_mode: "out",
-              bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
-          interactivity: {
-            detect_on: "canvas",
-            events: {
-              onhover: {
-                enable: false,
-                mode: "grab",
-              },
-              onclick: {
-                enable: true,
-                mode: "push",
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
+    <Link to="/home">
+      <ParticlesContainer>
+        <Quote>
+          <WordCaps>"T</WordCaps>he man who moves a
+          <MainWord className="word-1"> mountain</MainWord> <br />
+          &nbsp;<WordCaps className="caps-2">b</WordCaps>egins by carrying away
+          small
+          <MainWord className="word-2"> stones</MainWord>
+          <WordCaps>"</WordCaps>
+        </Quote>
+
+        <p>- confucius</p>
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 800,
                 },
               },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
+              color: {
+                value: "#c5a47e",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 192.40944730386272,
+                color: "#c5a47e",
+                opacity: 1,
+                width: 0.6,
+              },
+              move: {
+                enable: true,
                 speed: 3,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
               },
             },
-          },
-          retina_detect: true,
-        }}
-      />
-    </ParticlesContainer>
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: false,
+                  mode: "grab",
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
+      </ParticlesContainer>
+    </Link>
   );
 };
 
