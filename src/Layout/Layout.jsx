d5d6b9e2ad98107590a68cba908714a3a56f47f9
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-10-22 16:57:11 +0400

    Fix Layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,11 +3,11 @@ import Header from "../Components/Header";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
       <Header />
       {children}
       <h1>footer</h1>
-    </div>
+    </>
   );
 };
 
