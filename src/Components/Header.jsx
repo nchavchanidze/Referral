1aeeced16a3295cebe38d6c9490baf745fe5b8c2
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-10-22 23:46:44 +0400

    Header Hamburger

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavDropdown, Dropdown, Container } from "react-bootstrap";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 
 import Logo from "../Assets/images/Logo.svg";
 import GeFlag from "../Assets/images/Icons/ge.png";
 import UsFlag from "../Assets/images/Icons/us.png";
 
 const Header = () => {
+  const [burger, setBurger] = useState(false);
+
+  const handleBurger = () => {
+    setBurger(!burger);
+  };
+
   return (
     <HeaderContainer>
       <Navbar expand="lg" className="p-0 w-100 flex-wrap">
@@ -16,7 +22,16 @@ const Header = () => {
             <img src={Logo} alt="" />
           </Link>
         </LogoWrapper>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleBurger}>
+          <Hamburger
+            className={`hamburger ${burger ? "is-active" : ""}`}
+            id="hamburger-6"
+          >
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </Hamburger>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
             <li className="nav-item">
@@ -138,19 +153,57 @@ const LogoWrapper = styled.div`
   }
 `;
 
-const NavbarContainer = styled.nav`
-  padding-top: 0;
-  padding-bottom: 0;
-
-  ul {
-    align-items: center;
-    justify-content: space-between;
-    gap: 40px;
-  }
-`;
-
 const Flag = styled.img`
   width: 100%;
+`;
+
+const Hamburger = styled.div`
+  &.is-active {
+    -webkit-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    -webkit-transition-delay: 0.6s;
+    -o-transition-delay: 0.6s;
+    transition-delay: 0.6s;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+    .line {
+      &:nth-child(2) {
+        width: 0px;
+      }
+      &:nth-child(1) {
+        -webkit-transition-delay: 0.3s;
+        -o-transition-delay: 0.3s;
+        transition-delay: 0.3s;
+        -webkit-transform: translateY(5px);
+        -ms-transform: translateY(5px);
+        -o-transform: translateY(5px);
+        transform: translateY(5px);
+      }
+      &:nth-child(3) {
+        -webkit-transition-delay: 0.3s;
+        -o-transition-delay: 0.3s;
+        transition-delay: 0.3s;
+        -webkit-transform: translateY(-11px) rotate(90deg);
+        -ms-transform: translateY(-11px) rotate(90deg);
+        -o-transform: translateY(-11px) rotate(90deg);
+        transform: translateY(-11px) rotate(90deg);
+      }
+    }
+  }
+  .line {
+    width: 30px;
+    height: 2px;
+    background-color: #ecf0f1;
+    display: block;
+    /* margin: 6px auto; */
+    margin-top: 6px;
+    -webkit-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+  }
 `;
 
 export default Header;
