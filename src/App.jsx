9326733273cd95ea5e83499a9b63b1a7005725b0
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-10-27 19:17:50 +0400

    Components Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,79 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import Layout from "./Layout/Layout";
 import Confucius from "./Pages/Confucius";
-import Home from "./Pages/Home";
 
-import React from "react";
-import About from "./Components/About";
-import Services from "./Components/Services";
-import Contact from "./Components/Contact";
 import ComponentLayout from "./Layout/ComponentLayout";
+const Home = React.lazy(() => import("./Pages/Home"));
+const About = React.lazy(() => import("./Components/About"));
+const Services = React.lazy(() => import("./Components/Services"));
+const Contact = React.lazy(() => import("./Components/Contact"));
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Confucius} />
-        <Route
-          exact
-          path="/home"
-          render={() => (
-            <Layout>
-              <Home />
-            </Layout>
-          )}
-        />
-      </Switch>
-      <Route
-        exact
-        path="/about"
-        render={() => (
-          <ComponentLayout>
-            <Section>
-              <About />
-            </Section>
-          </ComponentLayout>
-        )}
-      />
-      <Route
-        exact
-        path="/services"
-        render={() => (
-          <ComponentLayout>
-            <Section>
-              <Services />
-            </Section>
-          </ComponentLayout>
-        )}
-      />
-      <Route
-        exact
-        path="/contact"
-        render={() => (
-          <ComponentLayout>
-            <Section>
-              <Contact />
-            </Section>
-          </ComponentLayout>
-        )}
-      />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              height: "100vh",
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1 style={{ fontSize: 100 }}>Loading Home</h1>
+          </div>
+        }
+      >
+        <Switch>
+          <Route exact path="/" component={Confucius} />
+          <Route
+            exact
+            path="/home"
+            render={() => (
+              <Layout>
+                <Home />
+              </Layout>
+            )}
+          />
+          <Route
+            exact
+            path="/about"
+            render={() => (
+              <ComponentLayout>
+                <Section>
+                  <About />
+                </Section>
+              </ComponentLayout>
+            )}
+          />
+          <Route
+            exact
+            path="/services"
+            render={() => (
+              <ComponentLayout>
+                <Section>
+                  <Services />
+                </Section>
+              </ComponentLayout>
+            )}
+          />
+          <Route
+            exact
+            path="/contact"
+            render={() => (
+              <ComponentLayout>
+                <Section>
+                  <Contact />
+                </Section>
+              </ComponentLayout>
+            )}
+          />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
