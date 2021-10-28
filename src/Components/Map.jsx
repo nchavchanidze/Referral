b2c3ab158afdb46f508d970e41daba67f45bd1fc
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-10-28 19:23:13 +0400

    Fix Map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -7,11 +7,11 @@ const Map = () => {
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.167638071344!2d44.77858631575139!3d41.738476882118604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40447294cd8c4faf%3A0xb98074af2e79b7b2!2z4YOT4YOY4YOT4YOj4YOR4YOUIOGDnuGDmuGDkOGDluGDkA!5e0!3m2!1ska!2sge!4v1632752074253!5m2!1ska!2sge"
       width="100%"
       height="450"
-      frameborder="0"
+      frameBorder="0"
       style={{border:0}}
-      allowfullscreen=""
+      allowFullScreen=""
       aria-hidden="false"
-      tabindex="0"
+      tabIndex="0"
       title="Referral Location"
     ></iframe>
   );
